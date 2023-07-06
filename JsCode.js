ddec0d3e18a62cc389e9ad84054cab0b7883c7b6
Avinash-0514 <100390020+Avinash-0514@users.noreply.github.com>
2023-07-06 10:19:05 +0530

    Update JsCode.js
    Added Exercises to the code

--- JsCode.js
+++ JsCode.js
@@ -13,5 +13,38 @@ document.getElementById("time").innerHTML = currTime;
 setTimeout(todayFun,5000);
 }
 todayFun();
-=========================
+*************************************************************************************
+2.Write a JavaScript program to get the current date.  
+Expected Output :
+mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+=============
+function getDate(){
+const time = new Date();
+let a = time.getDate(),b = time.getMonth(),c=time.getFullYear("");
+document.getElementById("dates").innerHTML = a+":"+b+":"+c;
+console.log(a,b,c);
+}
+getDate();
+***************************************************************************************	
+3.Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7. 
+======================
+function getAreaOfTriangle(){
+let a = 5,b=6,c=7;
+let triangle= a+b+c/2;
+document.getElementById("A-of-Tri").innerHTML = triangle;
+console.log(triangle);
+}
+getAreaOfTriangle();
+*******************************************************************************************
+4.Write a JavaScript program to rotate the string 'w3resource' in the right direction. This is done by periodically removing one letter from the string end and attaching it to the front.
+=========================  
+function getRotated(){
+let arrRotate = ['w','3','r','e','s','o','u','r','c','e'];
 
+let reversed = arrRotate.reverse(),joinedString =reversed.join('');
+
+console.log(reversed);
+document.getElementById("rotate").textContent = joinedString
+}
+******************************************
+getRotated();
